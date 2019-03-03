4cfac37affb1936817601f709f89b3a68e4fd7be
Nirmal <nirmalvettiankal@gmail.com>
2019-03-03 04:20:44 -0500

    specify threads & exception handling

--- src/main/java/com/vettiankal/Profiler.java
+++ src/main/java/com/vettiankal/Profiler.java
@@ -10,26 +10,28 @@ public class Profiler extends TimerTask {
     private long pollInterval;
     private long duration;
     private HashMap<ThreadInfo, ExecutionTree> trees;
+    private Thread[] threads;
     private int polls;
 
     private Timer scheduler;
     private long start;
     private ProfileCompleteEvent onComplete;
+    private Thread.UncaughtExceptionHandler exceptionHandler = (t, e) -> e.printStackTrace();
 
-    // TODO allow threads to monitor to be specified
-    public Profiler() {
-        this(10);
+    public Profiler(Thread... threads) {
+        this(10, threads);
     }
 
-    public Profiler(long pollInterval) {
-        this(pollInterval, -1);
+    public Profiler(long pollInterval, Thread... threads) {
+        this(pollInterval, -1, threads);
     }
 
-    public Profiler(long pollInterval, long duration) {
+    public Profiler(long pollInterval, long duration, Thread... threads) {
         this.pollInterval = pollInterval;
         this.duration = duration;
         this.trees = new HashMap<>();
         this.polls = 0;
+        this.threads = threads;
     }
 
     public long getPollingDelay() {
@@ -40,6 +42,18 @@ public class Profiler extends TimerTask {
         return duration;
     }
 
+    public long getTotalPolls() {
+        return polls;
+    }
+
+    public Thread.UncaughtExceptionHandler getExceptionHandler() {
+        return exceptionHandler;
+    }
+
+    public void setExceptionHandler(Thread.UncaughtExceptionHandler exceptionHandler) {
+        this.exceptionHandler = exceptionHandler;
+    }
+
     public void run() {
         try {
             if (duration > 0 && System.currentTimeMillis() - start > duration) {
@@ -48,7 +62,16 @@ public class Profiler extends TimerTask {
                 return;
             }
 
-            Map<Thread, StackTraceElement[]> stackTraces = Thread.getAllStackTraces();
+            Map<Thread, StackTraceElement[]> stackTraces;
+            if(threads.length > 0) {
+                stackTraces = new HashMap<>();
+                for(Thread thread : threads) {
+                    stackTraces.put(thread, thread.getStackTrace());
+                }
+            } else {
+                stackTraces = Thread.getAllStackTraces();
+            }
+
             for (Map.Entry<Thread, StackTraceElement[]> entry : stackTraces.entrySet()) {
                 ThreadInfo threadInfo = new ThreadInfo(entry.getKey());
                 if (trees.get(threadInfo) == null) {
@@ -61,8 +84,7 @@ public class Profiler extends TimerTask {
 
             this.polls++;
         } catch (Throwable e) {
-            // TODO set exception handler
-            e.printStackTrace();
+            exceptionHandler.uncaughtException(Thread.currentThread(), e);
         }
     }
 
@@ -73,7 +95,7 @@ public class Profiler extends TimerTask {
     public void start(ProfileCompleteEvent onComplete) throws ProfilerException {
         if(this.scheduler != null) throw new ProfilerException("Attempted to start already started profiler");
 
-        this.scheduler = new Timer();
+        this.scheduler = new Timer("Profiler", true);
         this.scheduler.scheduleAtFixedRate(this, 0, pollInterval);
         this.start = System.currentTimeMillis();
         this.onComplete = onComplete;
